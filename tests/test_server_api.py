a15bab007ef87701d4677a6a5c02f9601b6402c6
wang_tf <wang_tf@petalmail.com>
2026-03-17 17:42:33 +0800

    test(server_api): 移除硬编码的测试凭据并更新初始化测试
    将硬编码的环境变量从测试文件中移除，改为检查非空值

--- tests/test_server_api.py
+++ tests/test_server_api.py
@@ -5,10 +5,6 @@ import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scripts.server_api import ServerAPI
 
-# Set up environment variables for testing
-os.environ['CVAT_API_URL'] = 'http://10.205.252.247:8080/'
-os.environ['CVAT_USERNAME'] = 'wangtengfei'
-os.environ['CVAT_PASSWORD'] = 'wangtf@image123'
 
 class TestServerAPI:
     def setup_method(self):
@@ -17,11 +13,11 @@ class TestServerAPI:
     
     def test_initialization(self):
         """Test that ServerAPI initializes correctly"""
-        assert self.server_api.cvat_api_url == 'http://10.205.252.247:8080/'
-        assert self.server_api.cvat_username == 'wangtengfei'
-        assert self.server_api.cvat_password == 'wangtf@image123'
+        assert self.server_api.cvat_api_url is not None
+        assert self.server_api.cvat_username is not None
+        assert self.server_api.cvat_password is not None
         assert self.server_api.client is None
-    
+
     def test_get_server_info(self):
         """Test get_server_info method"""
         info = self.server_api.get_server_info()
